a49954ecd63fa5ead969fc8018b9399ae8c1f8c6
Pantawat <pantawat_b@cmu.ac.th>
2022-12-09 15:53:20 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,21 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int user,even,odd;
+    even = 0;
+    odd = 0;
+    do{
+        cout << "Enter an integer: ";
+        cin >> user;
+        if(user%2==0 && user != 0){
+            even++;
+        }
+        if(user%2 != 0 && user != 0){
+            odd++;
+    }
+       
+    }   while (user!=0);
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = "  << odd << endl;
     return 0;
 }
